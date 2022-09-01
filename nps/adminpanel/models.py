7d54d43ef7916a477fc2aeb3235f1d37781e2d06
dharmatejaneralla00 <dharmatejaneralla00@gmail.com>
2022-09-02 01:04:14 +0530

    login

--- nps/adminpanel/models.py
+++ nps/adminpanel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PatentModel(models.Model):
+    patenttype = models.CharField(max_length=30)
+
--- nps/login/views.py
+++ nps/login/views.py
@@ -1,48 +1,69 @@
 from django.contrib import messages
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from . import models
+
+
 # Create your views here.
 
 
-def loginUser(r):
+def loginuser(r):
     if not r.user.is_authenticated:
         if r.method == 'POST':
             username = r.POST['username']
             password = r.POST['password']
-            user = authenticate(username = username,password =password )
+            user = authenticate(username=username, password=password)
             if user:
-                login(r,user)
-                userModel = models.Usermodel.objects.get(username = username)
+                login(r, user)
+                userModel = models.Usermodel.objects.get(username=username)
                 team = userModel.teamname
                 if team == 'admin':
-                    return render(r,'adminpanel/')
+                    return render(r, 'adminpanel/')
             else:
-                messages.error(r,"user not found")
+                messages.error(r, "user not found")
     else:
         return
-    return render(r,'user/login.html')
+    return render(r, 'user/login.html')
 
-def registerUser(request):
+
+def registeruser(request):
     context = models.TeamModels.objects.all()
     if request.method == 'POST':
         email = request.POST['email']
         name = request.POST['name']
         password = request.POST['password']
         team = request.POST['team']
-        if not User.objects.filter(username = email).exists():
-            user = User.objects.create_user(username=email,email=email,password=password)
+        if not User.objects.filter(username=email).exists():
+            user = User.objects.create_user(username=email, email=email, password=password)
             user.first_name = name
             user.save()
-            user_auth = authenticate(username = email,password = password)
-            login(request,user_auth)
-            models.Usermodel(username = email,team = team).save()
-            return  redirect('home')
+            user_auth = authenticate(username=email, password=password)
+            login(request, user_auth)
+            models.Usermodel(username=email, team=team, name=name).save()
+            return redirect('home')
         else:
-            messages.error(request,"email already exists login to continue")
+            messages.error(request, "email already exists login to continue")
             return redirect('user/register')
-    return render(request,'user/register.html',{'con':context})
-def logoutUser(request):
+    return render(request, 'user/register.html', {'con': context})
+
+
+def logoutuser(request):
     logout(request)
     return redirect('login')
+
+
+def displayuser(r):
+    users = models.Usermodel.objects.all()
+    return render(r, 'user/displayuser.html', {'user': users})
+
+
+def resetpassword(r, username):
+    if r.method == 'POST':
+        user = User.objects.get(username=username)
+        newpass = r.POST['password']
+        user.set_password(newpass)
+        user.save()
+        messages.success(r, "password reset successfully")
+        return redirect('users/')
+    return render(r, 'users/resetpassword.html')
